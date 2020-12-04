44f2b715a8b6bc31e5ae45dbdcffbdc58fb1a2e2
valschmidt <vschmidt@ccom.unh.edu>
2020-12-04 13:59:01 -0800

    Adde directories for test scripts.

---
+++ cora_helm/scripts/pid_tester.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+import time
+import numpy as np
+import datetime
+import sys
+
+rospy.init_node('tester',anonymous=True)
+pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
+
+def publish(pub,T,duration):
+    s = datetime.datetime.now()
+    print(T)
+
+    while ((datetime.datetime.now() - s) < datetime.timedelta(seconds=duration) 
+           and not rospy.is_shutdown()):
+        pub.publish(T)
+        time.sleep(0.1)
+
+
+
+T = Twist()
+T.linear.x = 10
+T.angular.z = 0
+duration = 7.
+publish(pub,T,duration)
+'''
+duration = 10
+
+s = 5.
+
+time.sleep(.5)
+
+for s in np.arange(1,16,5.):
+    T.linear.x = s
+    for i in np.arange(0,.3,.05):
+        T.angular.z = i
+        publish(pub,T,duration)
+
+    for i in np.arange(-.3,0,.05):
+        T.angular.z=i
+        publish(pub,T,duration)
+'''
---
+++ dp_hover/scripts/dp_hover_tester.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+import actionlib
+import dp_hover.msg
+import tf.transformations
+import sys
+
+def test_client(x,y,yaw):
+
+    client = actionlib.SimpleActionClient('DP_hover_action',dp_hover.msg.dp_hoverAction)
+    client.wait_for_server()
+    print("found dp_action server")
+    target = PoseStamped()
+    target.pose.position.x = x
+    target.pose.position.y = y
+
+    quaternion = tf.transformations.quaternion_from_euler(0.0, 0.0, yaw)
+    target.pose.orientation.x = quaternion[0]
+    target.pose.orientation.y = quaternion[1]
+    target.pose.orientation.z = quaternion[2]
+    target.pose.orientation.w = quaternion[3]
+
+    print("sending:")
+    print(target)
+    goal = dp_hover.msg.dp_hoverGoal(target)
+    client.send_goal(goal)
+    print("waiting for result.")
+    client.wait_for_result()
+    return client.get_result()
+
+if __name__ == '__main__':
+
+    x = float(sys.argv[1])
+    y = float(sys.argv[2])
+    yaw = float(sys.argv[3])
+    #x = 300.0
+    #y = 0.0
+    #yaw = 0.0
+
+    try:
+        rospy.init_node('dp_tester')
+        result = test_client(x,y,yaw)
+        print("Result:", ', '.join([str(n) for n in result.sequence]))
+    except rospy.ROSInterruptException:
+        print("dp_Tester interrupted before completion.")
