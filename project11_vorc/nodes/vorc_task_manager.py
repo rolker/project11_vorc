29b6797048c5c08819d02555ce1ea2306b8069c3
Roland Arsenault <roland@ccom.unh.edu>
2020-12-04 13:56:47 -0500

    Improve info display sent to camp from task manager

--- project11_vorc/nodes/vorc_task_manager.py
+++ project11_vorc/nodes/vorc_task_manager.py
@@ -9,12 +9,13 @@ import rospy
 import actionlib
 
 from vrx_gazebo.msg import Task
-from std_msgs.msg import Float64, Float64MultiArray
+from std_msgs.msg import Float64, Float64MultiArray, String
 from geographic_msgs.msg import GeoPoseStamped, GeoPoint, GeoPath
 from geometry_msgs.msg import PoseStamped, Twist
 from marine_msgs.msg import Heartbeat
 from marine_msgs.msg import KeyValue
 from geographic_visualization_msgs.msg import GeoVizItem, GeoVizPointList
+from nav_msgs.msg import Odometry
 
 from robot_localization.srv import *
 
@@ -38,6 +39,29 @@ def task_callback(data):
 
 rospy.Subscriber('/vorc/task/info', Task, task_callback)
 
+odometry = None
+def odometry_callback(data):
+    global odometry
+    odometry = data
+
+rospy.Subscriber('/cora/robot_localization/odometry/filtered', Odometry, odometry_callback)
+
+cmd_vel = None
+def cmd_vel_callback(data):
+    global cmd_vel
+    cmd_vel = data
+
+rospy.Subscriber('/cmd_vel', Twist, cmd_vel_callback)
+
+# allow joystick override by listening to piloting mode
+piloting_mode = None
+
+def piloting_mode_callback(data):
+    global piloting_mode
+    piloting_mode = data.data
+    
+rospy.Subscriber('/project11/piloting_mode', String, piloting_mode_callback)
+
 null_twist = Twist()
 cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
     
@@ -47,7 +71,8 @@ def timer_callback(event):
     
     publishStatus()
     if status == 'idle':
-        cmd_vel_publisher.publish(null_twist)
+        if piloting_mode != 'manual':
+            cmd_vel_publisher.publish(null_twist)
         
         goal = None
 
@@ -55,26 +80,25 @@ def timer_callback(event):
             goal = station_keeping_goal
         
         if wayfinding_waypoints is not None:
-            if wayfinding_current_waypoint is None or wayfinding_current_waypoint+1 >= len(wayfinding_waypoints.poses):
+            if wayfinding_current_waypoint is None or wayfinding_current_waypoint+1 >= len(wayfinding_waypoints):
                 wayfinding_current_waypoint = 0
             else:
                 wayfinding_current_waypoint += 1
-            goal = wayfinding_waypoints.poses[wayfinding_current_waypoint]
+            goal = wayfinding_waypoints[wayfinding_current_waypoint]
             
         if goal is not None:
-            goal_map = fromLL(goal.pose.position.latitude, goal.pose.position.longitude, goal.pose.position.altitude)
             
             hg = dp_hover.msg.dp_hoverGoal()
             hg.target.header.frame_id = 'map'
             hg.target.header.stamp = rospy.get_rostime()
-            hg.target.pose.position = goal_map
-            hg.target.pose.orientation = goal.pose.orientation
+            hg.target.pose.position = goal['position']
+            hg.target.pose.orientation = goal['orientation']
             
             dp_hover_action_client.wait_for_server()
             dp_hover_action_client.send_goal(hg,dp_hover_done_callback, None, dp_hover_feedback_callback)
             
             status = 'dp_hover'
-            
+        
             #mbg = move_base_msgs.msg.MoveBaseGoal()
             #mbg.target_pose.header.frame_id = 'map'
             #mbg.target_pose.header.stamp = rospy.get_rostime()
@@ -84,66 +108,13 @@ def timer_callback(event):
             #move_base_action_client.wait_for_server()
             #move_base_action_client.send_goal(mbg,move_base_done_callback, None, move_base_feedback_callback)
             #status = 'move_base'
+            
+    if status == 'dp_hover':
+        if task is not None and task.name == 'wayfinding':
+            if dp_feedback is not None and dp_feedback.range < 1.0:
+                status = 'idle' # trigger jump to next waypoint
 
 rospy.Timer(rospy.Duration(.2), timer_callback)
-
-#
-# Task 1 - Station Keeping
-#
-
-station_keeping_goal = None
-station_keeping_pose_error = None
-station_keeping_rms_error = None
-
-def station_keeping_goal_callback(data):
-    global station_keeping_goal
-    
-    station_keeping_goal = data
-    markPosition(data.pose.position, 'station_keeping_goal')
-    
-
-rospy.Subscriber('/vorc/station_keeping/goal', GeoPoseStamped, station_keeping_goal_callback)
-        
-def station_keeping_pose_error_callback(data):
-    global station_keeping_pose_error
-    station_keeping_pose_error = data.data
-
-rospy.Subscriber('/vorc/station_keeping/pose_error', Float64, station_keeping_pose_error_callback)
-
-def station_keeping_rms_error_callback(data):
-    global station_keeping_rms_error
-    station_keeping_rms_error = data.data
-
-rospy.Subscriber('/vorc/station_keeping/rms_error', Float64, station_keeping_rms_error_callback)
-
-#
-# Task 2 - Wayfinding
-#
-
-wayfinding_waypoints = None
-wayfinding_min_errors = None
-wayfinding_mean_error = None
-wayfinding_current_waypoint = None
-
-def wayfinding_waypoints_callback(data):
-    global wayfinding_waypoints
-    global status
-    
-    wayfinding_waypoints = data
-    for i in range(len(data.poses)):
-        markPosition(data.poses[i].pose.position, 'waypoint_'+str(i))
-
-rospy.Subscriber('/vorc/wayfinding/waypoints', GeoPath, wayfinding_waypoints_callback)
-
-def wayfinding_min_errors_callback(data):
-    global wayfinding_min_errors
-    wayfinding_min_errors = data.data
-    
-rospy.Subscriber('/vorc/wayfinding/min_errors', Float64MultiArray, wayfinding_min_errors_callback)
-
-def wayfinding_mean_error_callback(data):
-    global wayfinding_mean_error
-    wayfinding_mean_error = data.data
 
 #
 # move_base stuff
@@ -161,13 +132,15 @@ def move_base_feedback_callback(feedback):
 # dp_hover stuff
 #
 dp_hover_action_client = actionlib.SimpleActionClient('DP_hover_action', dp_hover.msg.dp_hoverAction)
+dp_feedback = None
 
 def dp_hover_done_callback(state, result):
     global status
     status = 'idle'
 
 def dp_hover_feedback_callback(feedback):
-    pass
+    global dp_feedback
+    dp_feedback = feedback
 
 
 #
@@ -224,13 +197,26 @@ def publishStatus():
     if task is not None:
         hb.values.append(KeyValue('task',task.name))
         hb.values.append(KeyValue('state',task.state))
-        hb.values.append(KeyValue('elapsed_time',str(task.elapsed_time.secs)))
-        hb.values.append(KeyValue('remaining_time',str(task.remaining_time.secs)))
-        hb.values.append(KeyValue('score',str(task.score)))
-    
+        hb.values.append(KeyValue('time','elapsed: {0}, remaining: {1}'.format(task.elapsed_time.secs,task.remaining_time.secs)))
+        hb.values.append(KeyValue('score','{:.2f}'.format(task.score)))
+        
+    hb.values.append(KeyValue('---','---'))
+        
+    if odometry is not None:
+        hb.values.append(KeyValue('pose','{:.1f}, {:.1f} yaw: {:.2f}'.format(odometry.pose.pose.position.x,odometry.pose.pose.position.y,odometry.pose.pose.orientation.z)))
+        hb.values.append(KeyValue('twist','{:.1f}, {:.1f} yaw: {:.2f}'.format(odometry.twist.twist.linear.x,odometry.twist.twist.linear.y,odometry.twist.twist.angular.z)))
+        
+    if cmd_vel is not None:
+        hb.values.append(KeyValue('cmd_vel','{:.2f}, {:.1f} yaw: {:.2f}'.format(cmd_vel.linear.x,cmd_vel.linear.y,cmd_vel.angular.z)))
+        
+    if dp_feedback is not None:
+        hb.values.append(KeyValue('dp','range: {:.2f}, yaw err: {:.2f}'.format(dp_feedback.range, dp_feedback.yawerror)))
+
+    hb.values.append(KeyValue('---','---'))
+        
     if station_keeping_goal is not None:
-        hb.values.append(KeyValue('goal lat',str(station_keeping_goal.pose.position.latitude)))
-        hb.values.append(KeyValue('goal lon',str(station_keeping_goal.pose.position.longitude)))
+        hb.values.append(KeyValue('goal', '{:.1f}, {:.1f}'.format(station_keeping_goal['position'].x,station_keeping_goal['position'].y)))
+        
     if station_keeping_pose_error is not None:
         hb.values.append(KeyValue('pose_error',str(station_keeping_pose_error)))
     if station_keeping_rms_error is not None:
@@ -249,6 +235,77 @@ def publishStatus():
 
 status_publisher = rospy.Publisher('/project11/mission_manager/status', Heartbeat, queue_size = 1)
 display_publisher = rospy.Publisher('/project11/display', GeoVizItem, queue_size = 10)
+
+
+#
+# Task 1 - Station Keeping
+#
+
+station_keeping_goal = None
+station_keeping_pose_error = None
+station_keeping_rms_error = None
+
+def station_keeping_goal_callback(data):
+    global station_keeping_goal
+    
+    station_keeping_goal = {'position':fromLL(data.pose.position.latitude, data.pose.position.longitude, data.pose.position.altitude), 'orientation':data.pose.orientation}
+    markPosition(data.pose.position, 'station_keeping_goal')
+    
+
+rospy.Subscriber('/vorc/station_keeping/goal', GeoPoseStamped, station_keeping_goal_callback)
+        
+def station_keeping_pose_error_callback(data):
+    global station_keeping_pose_error
+    station_keeping_pose_error = data.data
+
+rospy.Subscriber('/vorc/station_keeping/pose_error', Float64, station_keeping_pose_error_callback)
+
+def station_keeping_rms_error_callback(data):
+    global station_keeping_rms_error
+    station_keeping_rms_error = data.data
+
+rospy.Subscriber('/vorc/station_keeping/rms_error', Float64, station_keeping_rms_error_callback)
+
+#
+# Task 2 - Wayfinding
+#
+
+wayfinding_waypoints = None
+wayfinding_min_errors = None
+wayfinding_mean_error = None
+wayfinding_current_waypoint = None
+
+def wayfinding_waypoints_callback(data):
+    global wayfinding_waypoints
+    global status
+    
+    wayfinding_waypoints = []
+    for i in range(len(data.poses)):
+        markPosition(data.poses[i].pose.position, 'waypoint_'+str(i))
+        wayfinding_waypoints.append({'position':fromLL(data.poses[i].pose.position.latitude, data.poses[i].pose.position.longitude, data.poses[i].pose.position.altitude), 'orientation':data.poses[i].pose.orientation})
+
+rospy.Subscriber('/vorc/wayfinding/waypoints', GeoPath, wayfinding_waypoints_callback)
+
+def wayfinding_min_errors_callback(data):
+    global wayfinding_min_errors
+    wayfinding_min_errors = data.data
+    
+rospy.Subscriber('/vorc/wayfinding/min_errors', Float64MultiArray, wayfinding_min_errors_callback)
+
+def wayfinding_mean_error_callback(data):
+    global wayfinding_mean_error
+    wayfinding_mean_error = data.data
+
+#
+# Task 3 - Landmark Localization and Characterization (perception)
+#
+
+#
+# Task 4 - Black box search (gymkhana)
+#
+
+
+
 
 
         
