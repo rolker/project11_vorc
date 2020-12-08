Fixed rotation of pinger and tuned kalman filter.

project11_vorc/nodes/vorc_task_manager.py
@@ -28,6 +28,9 @@ from usv_msgs.msg import RangeBearing
 import numpy as np
 
 from filterpy.kalman import KalmanFilter
+from filterpy.kalman import MerweScaledSigmaPoints
+from filterpy.kalman import UnscentedKalmanFilter
+
 from filterpy.common import Q_discrete_white_noise
 from task_manager_utilities import rotate
 
@@ -547,25 +550,33 @@ def initPingerTracker(X,R, isStationary = False):
 
     if isStationary:
             # Set up 1st order transition matrix.
-            pingerTracker.F = np.array([[1, dt, 0,  0, 0, 0],
-                      [0,  0, 0,  0, 0, 0],
-                      [0,  0, 1, dt, 0, 0],
-                      [0,  0, 0,  0, 0, 0],
-                      [0, 0, 0, 0, 1, dt],
-                      [0, 0, 0, 0, 0, 0]])
+            pingerTracker.F = np.array([[1., dt, 0.,  0., 0., 0.],
+                      [0.,  0., 0.,  0., 0., 0.],
+                      [0.,  0., 1., dt, 0., 0.],
+                      [0.,  0., 0.,  0., 0., 0.],
+                      [0., 0., 0., 0., 1, dt],
+                      [0., 0., 0., 0., 0., 0.]])
+            pingerTracker.F = np.array([[1., 0., 0.,  0., 0., 0.],
+                      [0.,  0., 0.,  0., 0., 0.],
+                      [0.,  0., 1., 0., 0., 0.],
+                      [0.,  0., 0.,  0., 0., 0.],
+                      [0., 0., 0., 0., 1, 0.],
+                      [0., 0., 0., 0., 0., 0.]])
     else:
         # Set up 1st order transition matrix.
-        pingerTracker.F = np.array([[1, dt, 0,  0, 0, 0],
-                            [0,  1, 0,  0, 0, 0],
-                            [0,  0, 1, dt, 0, 0],
-                            [0,  0, 0,  1, 0, 0],
-                            [0, 0, 0, 0, 1, dt],
-                            [0, 0, 0, 0, 0, 1]])
+        pingerTracker.F = np.array([[1, dt, 0.,  0., 0., 0.],
+                            [0., 1., 0.,  0., 0., 0.],
+                            [0., 0., 1., dt, 0., 0.],
+                            [0., 0., 0.,  1., 0., 0.],
+                            [0., 0., 0., 0., 1., dt],
+                            [0., 0., 0., 0., 0., 1.]])
     
     # Set up measurment matrix (only measuring position)
-    pingerTracker.H = np.array([[1, 0, 0, 0, 0, 0],
-                                [0, 0, 1, 0, 0, 0],
-                                [0, 0, 0, 0, 1, 0]])
+    pingerTracker.H = np.array([[1., 0., 0., 0., 0., 0.],
+                                [0., 0., 1., 0., 0., 0.],
+                                [0., 0., 0., 0., 1., 0.]])
+    # Set up measurment matrix (only measuring position)
+    # pingerTracker.H = np.eye(6)
     
     # Set up process model covariance (assumes update rate is constant)
     #qvar = ros.get_param("/task_manager/pinger/tracker_variance")
@@ -574,7 +585,7 @@ def initPingerTracker(X,R, isStationary = False):
         qvar = ros.get_param("/task_manager/pinger/tracker_variance")
     except:
         rospy.logwarn("Unable to retrieve pinger tracker variance from parameter server.")
-        rospy.logwarn("Setting default: 0.5")
+        rospy.logwarn("Setting default: 0.25")
         qvar = 0.5
         
     q = Q_discrete_white_noise(dim=2,dt=dt,var=qvar)
@@ -583,10 +594,14 @@ def initPingerTracker(X,R, isStationary = False):
     pingerTracker.Q = np.block([[ q, Z, Z],
                                 [Z, q, Z],
                                 [Z, Z, q]])
+    #pingerTracker.Q[1,1] = .01
+    #pingerTracker.Q[3,3] = .01
+    #pingerTracker.Q[5,5] = .01
     
     # Initalize filter with initial measurements. (increase uncertanty initially)
     pingerTracker.x = np.array([X[0],[0.0],X[1],[0.0],X[2],[0.0]])
-    pingerTracker.R = R * 3
+    pingerTracker.P = np.zeros((6,6))
+    pingerTracker.P[::2,::2] = R *3.0
     
 
 def updatePingerFilter(pingerMeasurement):
@@ -594,13 +609,15 @@ def updatePingerFilter(pingerMeasurement):
     if pingerTracker is None:
         return
     
+
     # Otherwise update the filter an incorprate a measurement if there's a new one.
     pingerTracker.predict()
+
     if pingerMeasurement is not None:
         # Assign the fields into the Kalman filter.
         pingerTracker.R = pingerMeasurement[1]
         pingerTracker.update(pingerMeasurement[0])
-        
+
         # Publish the measurement.
         pos = PoseWithCovarianceStamped()
         pos.header.stamp = rospy.Time.now()
@@ -622,7 +639,57 @@ def updatePingerFilter(pingerMeasurement):
     pos.pose.pose.position.z = pingerTracker.x_post[4]
     pos.pose.covariance = pingerTracker.P_post.flatten()
     pingerPubfiltered.publish(pos)
+    
+    ## WARNING ##
+    print(pingerTracker)
+    
     return pos
+
+
+
+def init_UKFPingerFilter(X,R):
+    global pingerTracker
+    
+    dt = 1.0
+    
+    def H_pingerUKF(x):
+        global pinger_location
+        
+        return pinger_location['position']
+    def F_pingerUKF(x,dt):
+        # Set up 1st order transition matrix.
+        F = np.array([[1., dt, 0., 0., 0., 0.],
+                    [0., 1., 0.,  0., 0., 0.],
+                    [0., 0., 1., dt, 0., 0.],
+                    [0., 0., 0.,  1., 0., 0.],
+                    [0., 0., 0., 0., 1., dt],
+                    [0., 0., 0., 0., 0., 1.]])
+        return F
+
+    points = MerweScaledSigmaPoints(n=6, alpha=0.1, beta=2., kappa=0.)
+    pingerTracker = UnscentedKalmanFilter(dim_x=6, dim_z=3, dt=dt, 
+                                          fx=F_pingerUKF,hx=H_pingerUKF,points=points)
+                                          
+    
+    try:
+        qvar = ros.get_param("/task_manager/pinger/tracker_variance")
+    except:
+        rospy.logwarn("Unable to retrieve pinger tracker variance from parameter server.")
+        rospy.logwarn("Setting default: 0.25")
+        qvar = 0.05
+        
+    q = Q_discrete_white_noise(dim=2,dt=dt,var=qvar)
+    
+    Z = np.zeros((2,2))
+    pingerTracker.Q = np.block([[ q, Z, Z],
+                                [Z, q, Z],
+                                [Z, Z, q]])
+    
+    # Initalize filter with initial measurements. (increase uncertanty initially)
+    pingerTracker.x = np.array([X[0],[0.0],X[1],[0.0],X[2],[0.0]])
+    pingerTracker.P = np.zeros((6,6))
+    pingerTracker.P[::2,::2] = R *3.0
+    
     
 
 def pinger_callback(data):
@@ -646,7 +713,7 @@ def pinger_callback(data):
         t.transform.rotation.w = q[3]
 
         vt = tf2_geometry_msgs.do_transform_vector3(v, t)
-    
+
         pinger = PoseStamped()
         pinger.pose.position.x = vt.vector.x
         pinger.pose.position.y = vt.vector.y
@@ -655,23 +722,83 @@ def pinger_callback(data):
 
         pinger_map = tf2_geometry_msgs.do_transform_pose(pinger, transformation)
         pinger_location['position'] = pinger_map.pose.position
-        
-        # Rotation with uncertainty example:
-        xyz, Cxyz = rotate.rangeBearingElevationtoXYZ(range=data.range,
-                                               bearing=-data.bearing, 
-                                               elevation=data.elevation,
-                                               sigmaRange = sigmaRange,
-                                               sigmaBearing = sigmaBearing,
-                                               sigmaElevation = sigmaElevation)
-        
-        print 'vt:', vt.vector.x, vt.vector.y, vt.vector.z, 'xyz', xyz[0][0],xyz[1][0],xyz[2][0]
 
+        tf_pinger_to_baselink = tf_buffer.lookup_transform('cora/pinger','cora/base_link',data.header.stamp, rospy.Duration(1.0))
+        #print(tf_pinger_to_baselink)
+        
+        # Rotation pinger location with uncertainty into base_link.:
+        xyz, Cxyz = rotate.rangeBearingElevationtoXYZ(range=data.range,
+                                                bearing=-data.bearing, 
+                                                elevation=data.elevation,
+                                                sigmaRange = sigmaRange,
+                                                sigmaBearing = sigmaBearing,
+                                                sigmaElevation = sigmaElevation)
+ 
+        
+        # These are not directly comparable. vt is in map, xyz is in base_link.
+        #print 'vt:', vt.vector.x, vt.vector.y, vt.vector.z, 'xyz', xyz[0][0],xyz[1][0],xyz[2][0]
+
+
+        # Get the transform from base_link to map.        
+        tf_baselink_to_map = tf_buffer.lookup_transform('cora/base_link','map',data.header.stamp, rospy.Duration(1.0))
+        tf_map_to_baselink = tf_buffer.lookup_transform('map','cora/base_link',data.header.stamp, rospy.Duration(1.0))
+
+        
+        #print(tf_baselink_to_map)
+        #print(tf_map_to_baselink)
+        
+        # Extract rpy.
+        rpy = tf.transformations.euler_from_quaternion([tf_baselink_to_map.transform.rotation.x,
+                                                        tf_baselink_to_map.transform.rotation.y,
+                                                        tf_baselink_to_map.transform.rotation.z,
+                                                        tf_baselink_to_map.transform.rotation.w])
+        
+        # Here we have a conundrum. We can get the transform from tf, but tf does not have an uncertainty.
+        # But we do have uncertainty in the odometry message for this transform. But this won't, be 
+        # time-synched in the way that other tf messages are. So for now, under the assumption that the 
+        # uncertainty is not changing quickly, I'm going to use that uncertainty.
+        Crpy_baselink = np.array(odometry.pose.covariance).reshape((6,6))[3:,3:]  # Gets just the roation portion of the covariance. 
+        Cxyz_baselink = np.array(odometry.pose.covariance).reshape((6,6))[:3,:3] # Gets just the position portion of the covariance.
+        
+
+        ## CONFUSION!! ##
+        # The map convention is X-East, Y-North. Makes sense. 
+        # The base_link convention is X-Forward, Y-Left. CONFUSING! 
+        # So after the decompose the pinger position measurement from the 
+        # pinger's reference frame into the vehicle's reference frame,
+        # we have to swap coordinates to match the convention of the 'map' frame
+        # before decomposing the coordinates into map coordinates. 
+        xyz = [-xyz[1], xyz[0], xyz[2]]
+        
+
+        xyz, Cxyz = rotate.rotateXYZ_RPH(xyz,Cxyz,np.array(rpy),Crpy_baselink)
+
+        ## WARNING HACK!!! ##
+        # After looking at this for way too long it is still not clear to me
+        # why this is necessary, but one must swap the x,y coordinates to get
+        # the proper result. Not satisfying. But it's working. 
+        xyz = np.array([xyz[1],xyz[0],xyz[2]])
+
+        # Finally add the vehicle's position (which came in the transform message).
+        ## LOOKOUT! One must use the translation data from the map_to_baselink transform,
+        # but NOT the base_link_to_map transform. They are not the same, because in ROS, you 
+        # always translate first, then rotate. So the translation data in the base_link
+        # transform is in the base_link (vessel) reference frame. 
+        
+        xyz = xyz + [[tf_map_to_baselink.transform.translation.x], 
+                        [tf_map_to_baselink.transform.translation.y],
+                        [tf_map_to_baselink.transform.translation.z]]
+        
+        
+        Cxyz = Cxyz + Cxyz_baselink
+        
+        
         if pingerTracker is None:
             initPingerTracker(xyz, Cxyz,isStationary=True)
         else:
-            pinger_filtered = updatePingerFilter((xyz,Cxyz))
-            pinger_filtered_map = tf2_geometry_msgs.do_transform_pose(pinger_filtered.pose, transformation)
-            pinger_location['position_filtered'] = pinger_filtered_map.pose.position
+            pinger_filtered_map = updatePingerFilter((xyz,Cxyz))
+            #pinger_filtered_map = tf2_geometry_msgs.do_transform_pose(pinger_filtered.pose, transformation)
+            pinger_location['position_filtered'] = pinger_filtered_map.pose.pose.position
 
     except Exception as e: #(tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
         print 'transformation exception:',e

project11_vorc/src/task_manager_utilities/rotate.py
@@ -31,7 +31,7 @@ def rangeBearingElevationtoXYZ(range, bearing, elevation, sigmaRange, sigmaBeari
 
     r = 0
     p = elevation
-    h = -bearing
+    h = bearing 
 
 
     sin_r = np.sin(r)
@@ -42,8 +42,8 @@ def rangeBearingElevationtoXYZ(range, bearing, elevation, sigmaRange, sigmaBeari
     cos_h = np.cos(h)
 
     # Angle covariance.
-    Crph = np.diag([sigmaElevation**2,
-                    0.0,git 
+    Crph = np.diag([0.0,
+                    sigmaElevation**2,
                     sigmaBearing**2])    
 
     # Build the combined covariance
@@ -84,7 +84,67 @@ def rangeBearingElevationtoXYZ(range, bearing, elevation, sigmaRange, sigmaBeari
     # Propagate the uncertainty
     #Cout = np.matmul(J,np.matmul(C,J.T))
     Cout = np.dot(J,np.dot(C,J.T))
-
     
     return (Xout, Cout)
+
+def rotateXYZ_RPH(X,Cxyz, R, Crph):
+
+    x = X[0][0]
+    y = X[1][0]
+    z = X[2][0]
+    
+    r = R[0]
+    p = R[1]
+    h = R[2]
+    
+    sin_r = np.sin(r)
+    cos_r = np.cos(r)
+    sin_p = np.sin(p)
+    cos_p = np.cos(p)
+    sin_h = np.sin(h)
+    cos_h = np.cos(h)
+    
+    # Build the combined covariance
+    C = np.zeros((6,6))
+    C[:3,:3] = Crph[:,:]
+    C[3:,3:] = Cxyz[:,:]
+
+    # Rotation Matrix.
+    M = np.array([[cos_p * cos_h, sin_r * sin_p * cos_h - cos_r * sin_h, cos_r * sin_p * cos_h+sin_r * sin_h],
+    [cos_p * sin_h, sin_r * sin_p * sin_h + cos_r * cos_h, cos_r * sin_p * sin_h-sin_r * cos_h],
+    [-sin_p, sin_r * cos_p, cos_r * cos_p]]);
+
+    # Build the Jacobian
+    J = np.zeros((3,6))
+    J[0,0] = sin_r  *  (y  *  sin_h - z  *  cos_h  *  sin_p) + cos_r  *  (y  *  cos_h  *  sin_p + z  *  sin_h)
+    J[0,1] = cos_h  *  (z  *  cos_r  *  cos_p + y  *  sin_r  *  cos_p - x  *  sin_p)
+    J[0,2] = z  *  cos_h  *  sin_r - (x  *  cos_p + y  *  sin_r  *  sin_p)  *  sin_h - cos_r  *  (y  *  cos_h + z  *  sin_p  *  sin_h)
+    #J[0,3] = M[0,0]
+    #J[0,4] = M[0,1]
+    #J(1,6,:,:) = M(1,3,:,:);
+    J[1,0] = cos_r  *  (y  *  sin_p  *  sin_h - z  *  cos_h) - sin_r  *  (y  *  cos_h + z  *  sin_p  *  sin_h)
+    J[1,1] = (z  *  cos_r  *  cos_p + y  *  sin_r  *  cos_p - x  *  sin_p)  *  sin_h
+    J[1,2] = x  *  cos_p  *  cos_h + cos_r  *  (z  *  cos_h  *  sin_p - y  *  sin_h) + sin_r  *  (y  *  cos_h  *  sin_p + z  *  sin_h)
+    #J(2,4,:,:) = M(2,1,:,:)
+    #J(2,5,:,:) = M(2,2,:,:)
+    #J(2,6,:,:) = M(2,3,:,:)
+    J[2,0] = cos_p  *  (y  *  cos_r - z  *  sin_r)
+    J[2,1] = -x  *  cos_p - (z  *  cos_r + y  *  sin_r)  *  sin_p
+    J[2,2] = 0.0
+    #J(3,4,:,:) = M(3,1,:,:);
+    #J(3,5,:,:) = M(3,2,:,:);
+    #J(3,6,:,:) = M(3,3,:,:);
+    J[:,3:] = M[:,:]
+
+    Xout = np.dot(M,X)
+
+
+    # Propagate the uncertainty
+    #Cout = np.matmul(J,np.matmul(C,J.T))
+    Cout = np.dot(J,np.dot(C,J.T))
+
+    
+    #Xout[0]=-X[0]
+    
+    return (Xout,Cout)
 